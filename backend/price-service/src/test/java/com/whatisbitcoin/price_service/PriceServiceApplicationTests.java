209341a8e90aedb42dd66a270bf91e5392f141dc
luizsolely <luizsolely@gmail.com>
2026-04-18 14:33:46 -0300

    fix: add test profiles to skip DB/RabbitMQ during CI

--- backend/price-service/src/test/java/com/whatisbitcoin/price_service/PriceServiceApplicationTests.java
+++ backend/price-service/src/test/java/com/whatisbitcoin/price_service/PriceServiceApplicationTests.java
@@ -2,12 +2,13 @@ package com.whatisbitcoin.price_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PriceServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
